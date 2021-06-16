d01e8f30bae15ab8895ff39d772f6de3a7457c7c
Dmitrii <31366100+Kanadets@users.noreply.github.com>
2021-06-16 02:33:05 -0700

    Request access + small improvments with animations

--- client/scripts/sripts.js
+++ client/scripts/sripts.js
@@ -1,6 +1,22 @@
 const emailHeaderForm = document.getElementById("header_email");
 const emailHeaderButton = document.getElementById("header_button");
+const emailHeaderDiv = document.getElementById("header-email-anim");
+const emailHeaderSent = document.getElementById("header-email-sent-anim");
 
 emailHeaderButton.addEventListener("click", (e) => {
   console.log(emailHeaderForm.value);
+  emailHeaderDiv.classList.toggle("hidden-email");
+
+  emailHeaderDiv.addEventListener("transitionend", () => {
+    emailHeaderDiv.classList.add("display-none");
+    emailHeaderSent.classList.add("email-sent-show");
+
+    setTimeout(() => {
+      emailHeaderSent.classList.remove("email-sent-show");
+      emailHeaderSent.addEventListener("transitionend", () => {
+        emailHeaderDiv.classList.remove("hidden-email");
+        emailHeaderDiv.classList.remove("display-none");
+      });
+    }, 2000);
+  });
 });

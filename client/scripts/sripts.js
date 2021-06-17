7c563f13971189a17ed007371f035b9690e9bcd0
Dmitrii <31366100+Kanadets@users.noreply.github.com>
2021-06-17 11:21:49 -0700

    Animations, and bug fixes

--- client/scripts/sripts.js
+++ client/scripts/sripts.js
@@ -2,21 +2,106 @@ const emailHeaderForm = document.getElementById("header_email");
 const emailHeaderButton = document.getElementById("header_button");
 const emailHeaderDiv = document.getElementById("header-email-anim");
 const emailHeaderSent = document.getElementById("header-email-sent-anim");
+const contactUsForm = document.getElementById("anim-contact-us-container");
+const contactUsFormSubmit = document.getElementById("anim-contact-us-submit");
+const submitContactUsBtn = document.getElementById("submit-contact-us-btn");
 
-emailHeaderButton.addEventListener("click", (e) => {
-  console.log(emailHeaderForm.value);
-  emailHeaderDiv.classList.toggle("hidden-email");
+const fullNameInputContact = document.getElementById("fullNameInputContact");
+const emailInputContact = document.getElementById("emailInputContact");
+const companyTypeInputContact = document.getElementById(
+  "companyTypeInputContact"
+);
+const companyNameInputContact = document.getElementById(
+  "companyNameInputContact"
+);
+const messageInputContact = document.getElementById("messageInputContact");
 
-  emailHeaderDiv.addEventListener("transitionend", () => {
-    emailHeaderDiv.classList.add("display-none");
-    emailHeaderSent.classList.add("email-sent-show");
+const forRestaurantsIntroEmailAnim = document.getElementById(
+  "for-restaurants-intro-email-anim"
+);
+const forRestaurantsInput = document.getElementById("for-restaurants-input");
+const forResForm = document.getElementById("for-res-form");
+const forRestaurantsBtn = document.getElementById("for-restaurants-btn");
+const forRestaurantsEmailSentAnimForRes = document.getElementById(
+  "for-restaurants-email-sent-anim-for-res"
+);
 
-    setTimeout(() => {
-      emailHeaderSent.classList.remove("email-sent-show");
-      emailHeaderSent.addEventListener("transitionend", () => {
-        emailHeaderDiv.classList.remove("hidden-email");
-        emailHeaderDiv.classList.remove("display-none");
-      });
-    }, 2000);
-  });
+emailHeaderButton.addEventListener("click", async (e) => {
+  const data = { to_email: emailHeaderForm.value };
+
+  const res = await sendEmail(data, "/send-email");
+
+  if (res) {
+    emailHeaderDiv.classList.add("hidden-email");
+
+    emailHeaderDiv.addEventListener("transitionend", () => {
+      emailHeaderDiv.classList.add("display-none");
+      emailHeaderSent.classList.add("email-sent-show");
+    });
+
+    // setInterval(() => {
+    //   emailHeaderSent.classList.remove("email-sent-show");
+    //   emailHeaderSent.addEventListener("transitionend", () => {
+    //     emailHeaderDiv.classList.remove("hidden-email");
+    //     emailHeaderDiv.classList.remove("display-none");
+    //   });
+    //   clearInterval();
+    // }, 5000);
+  }
 });
+
+submitContactUsBtn.addEventListener("click", async () => {
+  const data = {
+    fullName: fullNameInputContact.value,
+    to_email: emailInputContact.value,
+    companyType: companyTypeInputContact.value,
+    companyName: companyNameInputContact.value,
+    message: messageInputContact.value || "Message was not included",
+  };
+
+  const res = await sendEmail(data, "/send-form-email");
+
+  if (res) {
+    contactUsForm.classList.add("hide-contact-us-form");
+
+    contactUsForm.addEventListener("transitionend", () => {
+      contactUsForm.classList.add("display-none");
+      contactUsFormSubmit.classList.remove("display-none");
+      contactUsFormSubmit.classList.add("show-contact-us-form-submit");
+    });
+  }
+});
+
+forRestaurantsBtn.addEventListener("click", async () => {
+  const data = {
+    to_email: forRestaurantsInput.value,
+  };
+
+  const res = await sendEmail(data, "/send-email");
+
+  if (res) {
+    forRestaurantsIntroEmailAnim.classList.add(
+      "hide-for-restaurants-intro-email"
+    );
+
+    forRestaurantsIntroEmailAnim.addEventListener("transitionend", () => {
+      forResForm.classList.add("display-none");
+      forRestaurantsEmailSentAnimForRes.classList.add("email-sent-show");
+    });
+  }
+});
+
+const sendEmail = async (data, url) => {
+  try {
+    const res = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+    return res.ok;
+  } catch (e) {
+    return e;
+  }
+};
--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const nodemailer = require("nodemailer");
+const mongoose = require("mongoose");
+
+const Emails = require("./models/Emails");
 
 dotenv.config();
 
@@ -25,33 +28,90 @@ let smtpTransport = nodemailer.createTransport({
   },
 });
 
+mongoose.connect(process.env.DB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
+
+mongoose.connection.on("error", (error) => {
+  console.log("Failed to connect to DB: ", error);
+});
+
+mongoose.connection.on("connected", (error, res) => {
+  console.log("Mongoose is connected");
+});
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.post("/send-email", (request, response) => {
-  const { to_email, message, subject } = request.body;
+app.post("/send-email", async (request, response) => {
+  try {
+    const { to_email, message, subject } = request.body;
 
-  const templateParams = {
-    to_email,
-    message: "This is a test email",
-  };
+    const emailToAdd = await Emails.findOne({ email: to_email });
 
-  let mailOptions = {
-    from: "Knowbie Team <dev@getknowbie.com>",
-    to: to_email,
-    subject: subject,
-    html: `
-      <h1>Hello there</h1>
-    `,
-  };
+    if (emailToAdd) {
+      throw new Error("This email is already in wait list");
+    }
 
-  smtpTransport
-    .sendMail(mailOptions)
-    .then((res) =>
-      response.status(200).json({ message: "Message was sent successfully" })
-    )
-    .catch((err) => console.log(err));
+    const newMail = new Emails({ email: to_email });
+    await newMail.save();
+
+    let mailOptions = {
+      from: "Knowbie Team <dev@getknowbie.com>",
+      to: to_email,
+      subject: "What's next?",
+      html: `
+        <h1>Hello there</h1>
+      `,
+    };
+
+    smtpTransport
+      .sendMail(mailOptions)
+      .then((res) =>
+        response.status(200).json({ message: "Message was sent successfully" })
+      )
+      .catch((err) => console.log(err));
+  } catch (err) {
+    response.status(400).json({ message: "Something went wrong" });
+  }
+});
+
+app.post("/send-form-email", async (request, response) => {
+  try {
+    const {
+      fullName,
+      to_email,
+      companyType,
+      companyName,
+      message,
+    } = request.body;
+    let mailOptions = {
+      from: `Knowbie contact us form  <dev@getknowbie.com>"`,
+      to: "fomindmitrii12@gmail.com",
+      replyTo: to_email,
+      subject: "Contact",
+      html: `
+       <h4>Full name: ${fullName}</h4> 
+       <h4>Email: ${to_email}</h4> 
+       <h4>Company type: ${companyType}</h4> 
+       <h4>Company name: ${companyName}</h4> 
+        <p>${message}</p>
+      `,
+    };
+
+    smtpTransport
+      .sendMail(mailOptions)
+      .then((res) =>
+        response.status(200).json({ message: "Message was sent successfully" })
+      )
+      .catch((err) => console.log(err));
+  } catch (err) {
+    console.log(err);
+    response.status(400).json({ message: "Something went wrong" });
+  }
 });
 
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
